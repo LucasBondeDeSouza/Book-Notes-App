c0e28b669d4773959f828dd07791bc30636ead5f
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-08-15 16:13:43 -0300

    atualização

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 import express from "express"
 import rateLimit from 'express-rate-limit';
 import bodyParser from "body-parser"
-import pg from "pg"
+
 import bcrypt from "bcrypt"
 import session from "express-session"
 import passport from "passport"
 import { Strategy } from "passport-local"
 import GoogleStrategy from "passport-google-oauth2"
 import flash from "connect-flash"
-import env from "dotenv"
 import axios from "axios"
+
+import db from "./config/db.js"
 
 const app = express()
 const port = 3000
 const saltRounds = 10
-env.config()
-
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT
-})
-db.connect()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
