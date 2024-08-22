40d131d3eebf30913a02fcb0343fb837655fb2e4
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-08-22 18:31:00 -0300

    atualização

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ import { Strategy } from "passport-local"
 import GoogleStrategy from "passport-google-oauth2"
 import flash from "connect-flash"
 import axios from "axios"
-import ejs from 'ejs';
 
 import pool from "./config/db.js"
 
 const app = express()
 const port = 3000
 const saltRounds = 10
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
