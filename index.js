16f11c338d07c882515067fb877e99ed9bb3203c
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-08-22 18:18:30 -0300

    atualização

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { Strategy } from "passport-local"
 import GoogleStrategy from "passport-google-oauth2"
 import flash from "connect-flash"
 import axios from "axios"
+import ejs from 'ejs';
 
 import pool from "./config/db.js"
 
