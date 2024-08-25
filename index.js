dfc013991d6dbc1324965e410a254ab56118abc9
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-08-25 13:41:10 -0300

    atualização

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(
         resave: false,
         saveUninitialized: true,
         cookie: {
-            maxAge: 1000 * 60 * 60 * 24,
+            secure: false, maxAge: 1000 * 60 * 60 * 24, //
         },
     })
 )
