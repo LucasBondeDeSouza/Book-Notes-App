3c8ef062c046562b2ce659141533956c27a9292e
Lucas_Bonde <111398229+LucasBondeDeSouza@users.noreply.github.com>
2025-02-24 14:57:14 -0300

    atualização

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(50),
+    social_handle VARCHAR(50) UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100),
     picture TEXT
